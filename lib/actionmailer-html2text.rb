530f3fa985ab58dabf32ce08d968bf226f448824
Jevon Wright <jevon@powershop.co.nz>
2015-12-17 16:26:01 +1300

    Implement the connection from ActionMailer to Html2Text

--- lib/actionmailer-html2text.rb
+++ lib/actionmailer-html2text.rb
@@ -1,5 +1,26 @@
+require "html2text"
+
 module ActionMailer
   module Html2Text
-    # needs to be empty for ::VERSION in gemspec
+    TEXT_PLAIN = "text/plain"
+    TEXT_HTML = "text/html"
+
+    def collect_responses(headers)
+      add_plain_text(super(headers))
+    end
+
+    def add_plain_text(responses)
+      html_part = responses.detect { |response| response[:content_type] == TEXT_HTML }
+      text_part = responses.detect { |response| response[:content_type] == TEXT_PLAIN }
+
+      if html_part && !text_part
+        responses.insert 0, {
+          content_type: TEXT_PLAIN,
+          body: ::Html2Text.convert(html_part[:body])
+        }
+      end
+
+      responses
+    end
   end
 end
--- spec/html2text_spec.rb
+++ spec/html2text_spec.rb
@@ -3,15 +3,57 @@ require_relative "test_mailer"
 
 describe ActionMailer::Html2Text do
   let(:mailer) { Class.new(TestMailer) }
-  let(:mail) { mailer.test_email("test@example.com") }
+
+  let(:to_address) { "test@example.com" }
+  let(:mail) { mailer.test_email(to_address) }
+  let(:text) { mail.parts.first.body }
 
   context "by default" do
     it "is not a multipart email" do
       expect(mail).to_not be_multipart
+      expect(mail.parts).to be_empty
     end
   end
 
-  context "when defining our own multipart templates" do
+  context "when including our html2text module" do
+    let(:mailer) do
+      Class.new(TestMailer) do
+        include ActionMailer::Html2Text
+      end
+    end
+
+    it "is a multipart email" do
+      expect(mail).to be_multipart
+    end
+
+    it "has two parts" do
+      expect(mail.parts.size).to eq(2)
+    end
+
+    it "has text first" do
+      expect(mail.parts.first.content_type).to match("text/plain")
+    end
+
+    it "has html second" do
+      expect(mail.parts.second.content_type).to match("text/html")
+    end
+
+    context "the text part" do
+      it "does not have any HTML" do
+        expect(text).to_not include("<")
+      end
+
+      it "includes a markdown-style link" do
+        expect(text).to include("[your website](http://example.com)")
+      end
+
+      it "includes the to: address" do
+        expect(text).to include(to_address)
+      end
+    end
+  end
+
+  context "when defining our own multipart templates without Html2Text" do
     let(:mailer) do
       Class.new(TestMailer) do
         def template_name
@@ -34,6 +76,20 @@ describe ActionMailer::Html2Text do
 
     it "has html second" do
       expect(mail.parts.second.content_type).to match("text/html")
+    end
+
+    context "the text part" do
+      it "does not have any HTML" do
+        expect(text).to_not include("<")
+      end
+
+      it "includes a markdown-style link" do
+        expect(text).to include("[your website](http://example.com)")
+      end
+
+      it "includes the to: address" do
+        expect(text).to include(to_address)
+      end
     end
   end
 end
